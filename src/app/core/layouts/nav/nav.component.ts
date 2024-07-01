44fb1cad165c76eb5d6379f8f80eab55e49c3b25
Tomás Mendes <tomashm16@gmail.com>
2024-07-01 16:41:46 +0200

    feat: add navbar component

---
+++ src/app/core/layouts/nav/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+
+  siteName: string = 'Job Portal';
+}
