ec678e2132c2d824a089e439fa04bf533e1a598f
Tomás Mendes <tomashm16@gmail.com>
2024-07-01 16:44:07 +0200

    feat: import modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,27 @@ import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
 import { ToastModule } from 'primeng/toast';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './layouts/header/header.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @NgModule({
-  declarations: [CoreComponent],
-  imports: [CommonModule, CoreRoutingModule, ToastModule, FormsModule],
+  declarations: [
+    CoreComponent,
+    HeaderComponent,
+    NavComponent],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    ToastModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    InputIconModule,
+    IconFieldModule
+  ],
 })
 export class CoreModule {}
