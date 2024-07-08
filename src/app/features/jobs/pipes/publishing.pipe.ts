529d6e6bfa08bc73da6b6ad7c36df5cec1c08986
lopidurs <nathan.sancke@hotmail.fr>
2024-07-08 16:09:57 +0200

    Feat(jobs): add traduction

--- src/app/features/jobs/pipes/publishing.pipe.ts
+++ src/app/features/jobs/pipes/publishing.pipe.ts
@@ -1,21 +1,27 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
-  name: 'publishingPipe'
+  name: 'publishingPipe',
+  pure: false
 })
 export class PublishingPipe implements PipeTransform {
+
+  private readonly translate = inject(TranslateService);
 
   transform(value: Date): string {
     const now = new Date();
     const diff = now.getTime() - value.getTime();
     const diffInDays = diff / (1000 * 3600 * 24);
 
+
     if (diffInDays < 1) {
-      return 'Today';
+      return this.translate.instant('job.today');
     }
+      
 
     if (diffInDays < 2) {
-      return 'Yesterday';
+      return this.translate.instant('job.yesterday');
     }
 
       return `${Math.floor(diffInDays)} days ago`;
