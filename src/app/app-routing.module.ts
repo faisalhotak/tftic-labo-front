12a6517eaf15cbbf04ce1169de5537b67f5549de
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-07-09 10:24:27 +0200

    feat: added form field component, added translations, updated forms with validation messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
     children: [
       {
         path: '',
         loadChildren: () =>
           import('./core/core.module').then((m) => m.CoreModule),
-      }
+      },
     ],
   },
 ];
