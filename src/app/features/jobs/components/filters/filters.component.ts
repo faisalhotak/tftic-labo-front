c11b42b2e4d1bf1532833dad4850e496cc066953
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-07-09 12:12:16 +0200

    refactoring: refactored FiltersComponent with dependency injection with inject() instead of constructor

--- src/app/features/jobs/components/filters/filters.component.ts
+++ src/app/features/jobs/components/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { JobService } from '../../service/job.service';
 import { Observable } from 'rxjs';
 import { Job } from '../../models/job';
@@ -7,26 +7,24 @@ import { Pair } from '../../../../shared/models/pair';
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrl: './filters.component.scss'
+  styleUrl: './filters.component.scss',
 })
-export class FiltersComponent implements OnInit{
-  constructor(private readonly jobService: JobService) {
-  }
+export class FiltersComponent implements OnInit {
+  private readonly jobService = inject(JobService);
 
-  getJobs() {
-    this.jobs$ = this.jobService.getAllJobs(this.filters);
-    this.jobsToSend$.emit(this.jobs$);
-  }
-  
   jobs$!: Observable<Job[]>;
 
   filters: Map<string, string> = new Map<string, string>();
 
   @Output() jobsToSend$ = new EventEmitter<Observable<Job[]>>();
 
+  getJobs() {
+    this.jobs$ = this.jobService.getAllJobs(this.filters);
+    this.jobsToSend$.emit(this.jobs$);
+  }
+
   addFilter(filter: Pair) {
     this.filters.set(filter.key, filter.value);
-    console.log(this.filters);
     this.getJobs();
   }
 
